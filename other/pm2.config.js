b6190e513a7e3a63dc191b3a9ad233208b7d64c9
barclayd <barclaysd@me.com>
2023-01-09 20:37:59 +0000

    chore: add pm2

---
+++ other/pm2.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Remix',
+      script: 'remix dev',
+      ignore_watch: ['.'],
+      env: {
+        NODE_ENV: process.env.NODE_ENV ?? 'development',
+        FORCE_COLOR: '1'
+      }
+    },
+    {
+      name: 'Postcss',
+      script: 'postcss styles/**/*.css --base styles --dir app/styles',
+      autorestart: false,
+      watch: [
+        './tailwind.config.js',
+        './app/**/*.ts',
+        './app/**/*.tsx',
+        './styles/**/*.css'
+      ],
+      env: {
+        NODE_ENV: process.env.NODE_ENV ?? 'development',
+        FORCE_COLOR: '1'
+      }
+    }
+  ]
+};
